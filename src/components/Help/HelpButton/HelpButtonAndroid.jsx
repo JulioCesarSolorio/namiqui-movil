105341e41e0475586029942cf50c9cf90fd1b00f
Josue Rosales <rosales.chimal@gmail.com>
2021-06-14 20:15:34 -0500

    boton de ayuda JRC

--- src/components/Help/HelpButton/HelpButtonAndroid.jsx
+++ src/components/Help/HelpButton/HelpButtonAndroid.jsx
@@ -1,24 +1,14 @@
-/*import React, { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import {
   View, Platform, DeviceEventEmitter, Linking, AppState,
 } from 'react-native';
-import {
-  showFloatingBubble, requestPermission, checkPermission, initialize, hideFloatingBubble,
-} from 'react-native-floating-bubble';
+
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { colors } from '../../../style';
 import { NamiquiButton } from '../../styledComponents';
 
 export function closeHelpButton() {
-  if (Platform.OS === 'android') {
-    initialize().then(() => {
-      hideFloatingBubble().then(() => {
-        console.log('Boton de ayuda removido');
-      });
-    }).catch(() => {
-      console.log('no se logro inicializar el boton de ayuda');
-    });
-  }
+
 }
 
 export function showHelpButton() {
@@ -38,66 +28,7 @@ export function showHelpButton() {
 }
 
 export default function HelpButtonAndroid(props) {
-  function initHelpFloatingButton() {
-    // se verifica si se tiene el permiso para mostrar el boton de ayuda
-    checkPermission().then(() => {
-      // si no se tiene el permiso, solicitarlo
-      requestPermission().then(() => {
-        // una vez otorgado el permiso, mostrar el boton
-
-        console.log('permiso otorgado');
-
-        showHelpButton();
-      })
-        .catch(() => {
-          console.log('Es necesario el permiso para mostrar el boton de ayuda');
-        });
-    }).catch(() => {
-      // en caso de tenerlo mostrarlo
-      showHelpButton();
-    });
-  }
-
-  // eventos del boton de ayuda
-  useEffect(() => {
-    DeviceEventEmitter.addListener('floating-bubble-press', () => {
-      console.log('click boton');
-      console.log(AppState.currentState);
-
-      switch (AppState.currentState) {
-        case 'active':
-          props.navigation.navigate('Pedir Ayuda');
-          break;
-        case 'background':
-          Linking.openURL('namiqui://ayuda');
-          setTimeout(() => {
-            /*
-            NO USAR (Funciona pero da muchos errores)
-            props.navigation.reset({
-                index: 0,
-                routes: [{ name: 'Pedir Ayuda' }],
-            });
-            */
-
-            /*
-            props.navigation.navigate('Pedir Ayuda');
-          }, 500);
-          break;
-        case 'inactive':
-          // si esta en segundo plano se debe de abrir la app indicando que redireccione a la pantalla de ayuda
-          Linking.openURL('namiqui://ayuda');
-          break;
-        default:
-          props.navigation.navigate('Pedir Ayuda');
-      }
-    });
-
-    DeviceEventEmitter.addListener('floating-bubble-remove', () => {
-      console.log('Boton removido');
-    });
-    return () => { };
-  }, []);
-
+  
   return (
     <View style={{
       flexDirection: 'row',
@@ -120,15 +51,9 @@ export default function HelpButtonAndroid(props) {
         flex: 10,
       }}
       >
-        <NamiquiButton
-          onPress={() => initHelpFloatingButton()}
-          style={{ width: '100%', minWidth: 220 }}
-          textStyle={{ fontSize: 16 }}
-          text="Mostrar Boton Ayudame"
-        />
+        
       </View>
     </View>
 
   );
 }
-*/
