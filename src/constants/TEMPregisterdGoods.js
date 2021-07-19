8f81d7869402a724b8edba8710266cd1a5cbd053
Jeff Abney <jeffmabney@gmail.com>
2021-07-19 13:51:38 -0500

    Crea recursos temporales

---
+++ src/constants/TEMPregisterdGoods.js
@@ -0,0 +1,19 @@
+import redBike from '../assets/images/redBike.jpeg';
+import bikeSeat from '../assets/images/bikeSeat.jpg';
+import blueBike from '../assets/images/blueBike.jpg';
+import blueBikeNum from '../assets/images/blueBikeNum.jpeg';
+
+export default registeredGoods = [
+    {
+      name: 'Bici Papá',
+      value: 0,
+      description: 'BICICLETA APACHE ROJA RIN 24, CALCOMANIAS DE COCACOLA. ASIENTO DICE “TRUENO” LLANTA DELANTERA MARCA “RAPID”',
+      images: [redBike, bikeSeat, redBike, bikeSeat, redBike, bikeSeat],
+    },
+    {
+      name: 'Bici Mamá',
+      value: 1,
+      description: 'BICICLETA Cruiser Azul RIN 20, Canasta enfrent. numero de seríe 123456',
+      images: [blueBike, blueBikeNum],
+    },
+  ]
